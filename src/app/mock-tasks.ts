af14d83d8752d8ebf9ab34b006e223c001632cc1
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-12-26 20:22:38 -0300

    add tasks mockup

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Termnar de hacer la tarea',
+    day: 'Enero 5 a las 12:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Revisar y practicar Javascript',
+    day: 'Enero 5 a las 15:00',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Armar secciones en react!',
+    day: 'Enero 5 a las 18:00',
+    reminder: false,
+  },
+  {
+    id: 4,
+    text: 'Compartir mi sitio web',
+    day: 'Enero 5 a las 21:00',
+    reminder: false,
+  },
+];
