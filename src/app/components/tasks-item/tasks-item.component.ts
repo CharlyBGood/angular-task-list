cf5d4376255836c295c1cf2519caffa4da0a9e7b
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-12-27 11:20:09 -0300

    edit list items

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -6,9 +6,14 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-tasks-item',
   templateUrl: './tasks-item.component.html',
-  styleUrls: ['./tasks-item.component.css']
+  styleUrls: ['./tasks-item.component.css'],
 })
 export class TasksItemComponent {
   @Input() task: Task = TASKS[0];
   faTimes = faTimes;
+  color = "red"
+
+  onClick() {
+    alert("sure you want to delete entrie?")
+  }
 }
