d2dc683504d9aed73efd70be60e5f0235ddb5168
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-12-27 19:40:39 -0300

    clear routes behaviour and add some text on about page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent {
   showAddForm: boolean = false;
   subscription?: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService
       .onToggle()
       .subscribe((value) => (this.showAddForm = value));
@@ -20,5 +21,9 @@ export class HeaderComponent {
 
   toggleAddTask() {
     this.uiService.toggleAddForm();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
